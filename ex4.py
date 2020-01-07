872a374021d1568bcf022c47727a06dc4ec29997
jianning1 <42845009+jianning1@users.noreply.github.com>
2020-01-06 21:20:07 -0500

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,14 +1,14 @@
 # Exercise 4: Variables and Names
 
-cars = 100 #Total number of cars
-space_in_a_car = 4.0 #How many people can seat in a car
-drivers = 30 #Total number of drivers
-passengers = 90 #Total number of passengers
-cars_not_driven = cars - drivers #Total number of empty cars
-cars_driven = drivers #Total number of cars on the road
-carpool_capacity = cars_driven * space_in_a_car #Total spaces in cars on the road
+cars = 100 # Total number of cars
+space_in_a_car = 4.0 # How many people can seat in a car
+drivers = 30 # Total number of drivers
+passengers = 90 # Total number of passengers
+cars_not_driven = cars - drivers # Total number of empty cars
+cars_driven = drivers # Total number of cars on the road
+carpool_capacity = cars_driven * space_in_a_car # Total spaces in cars on the road
 average_passengers_per_car = passengers / cars_driven
-#Average number of passengers each car need to carry
+# Average number of passengers each car need to carry
 
 print("There are", cars, "cars available.")
 print("There are only", drivers, "drivers available.")
